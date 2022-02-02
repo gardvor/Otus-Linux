605ccd06bb9d9c4267eddea561a1a4dd85b2423a
gardvor <84384876+gardvor@users.noreply.github.com>
2022-02-02 10:09:55 -0500

    Add files via upload

--- Otus-Bash/check_log.sh
+++ Otus-Bash/check_log.sh
@@ -20,14 +20,14 @@ then
         timeLast=$(awk '{print $4 $5}' access-4560-644067.log | sed 's/\[//; s/\]//' | sed -n "$checkLines"p)
         # Запись количества строк в файле
         echo $checkLines > ./lines
-        # Определение количества IP запросов с IP адресов
+        # Определение количества запросов с IP адресов (Выводим адрес если запросов с адреса было от 5 и больше)
         IP=$(awk "NR>$checkLines"  access-4560-644067.log | awk '{print $1}' | sort | uniq -c | sort -rn | awk '{ if ( $1 >= 5 ) { print "Количество запросов:" $1, "IP:" $2 } }')
         # Y количества адресов
         addresses=$(awk '($9 ~ /200/)' access-4560-644067.log|awk '{print $7}'| sort |uniq -c | sort -rn | awk '{ if ( $1 >= 10 ) { print "Количество запросов:" $1, "URL:" $2 } }')
         # все ошибки c момента последнего запуска
         errors=$(cat access-4560-644067.log | cut -d '"' -f3 | cut -d ' ' -f2 | sort | uniq -c | sort -rn | awk '{ if ( $2 >= 400 ) { print "Колличество ошибок:" $1, "Код ошибки:" $2}}')
         # Отправка почты
-        echo -e "Данные за период:$timeHead-$timeLast\n$IP\n\n"Часто запрашиваемые адреса:"\n$addresses\n\n"Частые ошибки:"\n$errors" #| mail -s "NGINX Log Info" root@localhost
+        echo -e "Данные за период:$timeHead-$timeLast\n$IP\n\n"Часто запрашиваемые адреса:"\n$addresses\n\n"Частые ошибки:"\n$errors" | mail -s "NGINX Log Info" root@localhost
 
         rm -f "$lockfile"
         trap - INT TERM EXIT
@@ -36,7 +36,7 @@ then
         # Дата начала и конца
         timeHead=$(awk '{print $4 $5}' access-4560-644067.log | sed 's/\[//; s/\]//' | sed -n "$(($number+1))"p)
         timeLast=$(awk '{print $4 $5}' access-4560-644067.log | sed 's/\[//; s/\]//' | sed -n "$checkLines"p)
-        # Определение количества IP запросов с IP адресов
+        # Определение количества запросов с IP адресов (Выводим адрес если запросов с адреса было от 5 и больше)
         IP=$(awk "NR>$(($number+1))"  access-4560-644067.log | awk '{print $1}' | sort | uniq -c | sort -rn | awk '{ if ( $1 >= 5 ) { print "Количество запросов:" $1, "IP:" $2 } }')
         # Y количества адресов
         addresses=$(awk '($9 ~ /200/)' access-4560-644067.log|awk '{print $7}'|sort|uniq -c|sort -rn|awk '{ if ( $1 >= 10 ) { print "Количество запросов:" $1, "URL:" $2 } }')
@@ -45,7 +45,7 @@ then
         # Запись количества строк в файле
         echo $checkLines > ./lines
         # Отправка почты
-        echo -e "Данные за период:$timeHead-$timeLast\n$IP\n\n"Часто запрашиваемые адреса:"\n$addresses\n\n"Ошибки:"\n$errors" #| mail -s "NGINX Log Info" root@localhost
+        echo -e "Данные за период:$timeHead-$timeLast\n$IP\n\n"Часто запрашиваемые адреса:"\n$addresses\n\n"Ошибки:"\n$errors" | mail -s "NGINX Log Info" root@localhost
         
         rm -f "$lockfile"
         trap - INT TERM EXIT
