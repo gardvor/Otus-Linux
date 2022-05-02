c442ccd1f0c4eccb1c2b8323b392bdccf568b71c
gardvor <84384876+gardvor@users.noreply.github.com>
2022-05-02 09:42:55 +0300

    Add files via upload

--- Otus-Postgres/postgres-install.sh
+++ Otus-Postgres/postgres-install.sh
@@ -1,4 +1,6 @@
-sudo dnf module reset postgresql -y &&
-sudo dnf install @postgresql:12 -y &&
-postgresql-setup initdb &&
-systemctl enable postgresql --now 
+sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - &&
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list &&
+sudo apt update &&
+sudo apt -y install postgresql-12 postgresql-client-12
+sudo systemctl stop ufw
+sudo systemctl disable ufw
