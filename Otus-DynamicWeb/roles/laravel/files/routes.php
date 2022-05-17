e470fb658efa79afa09f5a7cb67d295a00aacffe
gardvor <84384876+gardvor@users.noreply.github.com>
2022-05-17 19:05:04 +0300

    Add files via upload

---
+++ Otus-DynamicWeb/roles/laravel/files/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+Route::get('homework', function () {
+    return '<h1>otus linux</h1>';
+});
